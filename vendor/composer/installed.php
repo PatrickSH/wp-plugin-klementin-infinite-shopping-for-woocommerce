1b1964d701aec894acdb2fe064262828344598e0
Patrick <patrick@swiftely.com>
2022-03-17 14:27:28 +0100

    Changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f2d3754d540227b1a2bdcc19f3d335d914f72bcc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f2d3754d540227b1a2bdcc19f3d335d914f72bcc',
+            'dev_requirement' => false,
+        ),
+        'klementinweb/klementin-wp' => array(
+            'pretty_version' => '1.1.2',
+            'version' => '1.1.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../klementinweb/klementin-wp',
+            'aliases' => array(),
+            'reference' => '18716ede354fcd2c75d4c023d445aa8fa6a79b92',
+            'dev_requirement' => false,
+        ),
+    ),
+);
